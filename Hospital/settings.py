67df7222764b60dba7878bd3775caa0559450d3f
unknown <nwosuemmanuel159@gmail.com>
2025-07-15 16:15:08 +0100

    14th Commt

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#z0z-yrk0m2g1931ou)8e99xfql352qm(#+%iq4j(roah7!$=#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "ivory-2k91.onrender.com",
