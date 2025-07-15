ee5d6868963bc5a3d694ee661cedecf31b995c5b
unknown <nwosuemmanuel159@gmail.com>
2025-07-15 02:23:02 +0100

    13th commit

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -135,6 +136,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "Hospital/static"]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
